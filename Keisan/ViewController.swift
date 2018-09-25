608475f388f98bde9e478c153e4884b5374a957d
modeverv <modeverv@gmail.com>
2018-09-25 23:54:54 +0900

    initial

--- Keisan/ViewController.swift
+++ Keisan/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var label: UILabel!
+
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    let value = 10 - 1
+    label.text = "10 - 1 = \(value)"
   }
 
 
